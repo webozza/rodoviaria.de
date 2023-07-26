6a7d2c4a6937c9c7e035b7202b0d883737eb0ab8
webozza <webozza@gmail.com>
2023-07-26 23:39:05 +0800

    version update

--- functions.php
+++ functions.php
@@ -208,22 +208,25 @@ function load_more_posts()
             $loop->the_post();
             ?>
             <!-- Your post markup here -->
-            <div class="single-post">
-                <?php $thumbnail_url =  get_the_post_thumbnail_url(get_the_ID(), 'full');
-                ?>
-                <img src="<?php echo $thumbnail_url; ?>" alt="Featured Image">
-                <div class="content-box">
-                    <div class="place-name">
-                        <p><?php the_title(); ?></p>
-                    </div>
-                    <div class="content">
-                        <p><?php include(get_template_directory() . '/content-loop.php'); ?></p>
-                    </div>
-                </div>
-                <a class="post-link" href="<?= get_permalink()?>">
-                    <div><img src="<?= get_template_directory_uri()?>/img/icons/botao.png " alt=""></div>
-                </a>
-            </div>
+			<div class="single-post">
+				<a href="<?= get_permalink() ?>">
+					<?php $thumbnail_url =  get_the_post_thumbnail_url(get_the_ID(), 'full');
+					?>
+					<img src="<?php echo $thumbnail_url; ?>" alt="Featured Image">
+					<div class="content-box">
+						<div class="place-name">
+							<p><?php the_title(); ?></p>
+						</div>
+						<div class="content">
+							<p><?php include(get_template_directory() . '/content-loop.php'); ?></p>
+						</div>
+					</div>
+					<a class="post-link" href="<?= get_permalink()?>">
+						<div><img src="<?= get_template_directory_uri()?>/img/icons/botao.png " alt=""></div>
+					</a>
+				</a>
+			</div>
+			
             <?php
         }
         wp_reset_postdata();
