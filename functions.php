e5b2ec779484d8513915221bd9708fe2e852d0e7
webozza <webozza@gmail.com>
2023-07-26 16:20:19 +0800

    version update

--- functions.php
+++ functions.php
@@ -206,7 +206,20 @@ function load_more_posts()
             ?>
             <!-- Your post markup here -->
             <div class="single-post">
-                <!-- Your post content here -->
+                <?php $thumbnail_url =  get_the_post_thumbnail_url(get_the_ID(), 'full');
+                ?>
+                <img src="<?php echo $thumbnail_url; ?>" alt="Featured Image">
+                <div class="content-box">
+                    <div class="place-name">
+                        <p><?php the_title(); ?></p>
+                    </div>
+                    <div class="content">
+                        <p><?php include(get_template_directory() . '/content-loop.php'); ?></p>
+                    </div>
+                </div>
+                <a class="post-link" href="<?= get_permalink()?>">
+                    <div><img src="<?= get_template_directory_uri()?>/img/icons/botao.png " alt=""></div>
+                </a>
             </div>
             <?php
         }
