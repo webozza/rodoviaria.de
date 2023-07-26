bc9868a3d7e5a9df4f7d9d8dae55092f19354f63
webozza <webozza@gmail.com>
2023-07-26 16:49:39 +0800

    version update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 $ = jQuery;
 $(document).ready(function () {
-  $('.question').click(function () {
-      var answer = $(this).next('.answer');
-      answer.slideToggle();
-      $('.answer').not(answer).slideUp();
-      // Icon Changing
-      var icon = $(this).find('i');
-      $('.fa.fa-chevron-up').not(icon).removeClass('fa fa-chevron-up').addClass('fa fa-chevron-down');
-      icon.toggleClass('fa fa-chevron-down fa fa-chevron-up');
+  $(".question").click(function () {
+    var answer = $(this).next(".answer");
+    answer.slideToggle();
+    $(".answer").not(answer).slideUp();
+    // Icon Changing
+    var icon = $(this).find("i");
+    $(".fa.fa-chevron-up")
+      .not(icon)
+      .removeClass("fa fa-chevron-up")
+      .addClass("fa fa-chevron-down");
+    icon.toggleClass("fa fa-chevron-down fa fa-chevron-up");
   });
+
+  let searchBar = async () => {
+    $(".search-field").after(
+      '<button><i class="fa-solid fa-magnifying-glass"></i></button>'
+    );
+    $(".post-search button").click(function () {
+      $(".search-form").submit();
+    });
+  };
+  searchBar();
 });
