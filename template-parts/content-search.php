21c7303f47b39d9a24bc4fc1beab8afbc9d94907
Sarah Chowdhury <talha@webozzza.com>
2023-07-28 10:57:14 +0600

    test

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -28,26 +28,4 @@
                     </a>
                 </a>
             </div>
-			
-	<!-- <header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			bruno_posted_on();
-			bruno_posted_by();
-			?>
-		</div>
-		<?php endif; ?>
-	</header> -->
-
-	<!-- <?php bruno_post_thumbnail(); ?>
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div> -->
-
-	<footer class="entry-footer">
-		<?php bruno_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
