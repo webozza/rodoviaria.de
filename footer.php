63966c2e9a62ceff9a37dedf52aca83ec6247d03
webozza <webozza@gmail.com>
2023-07-28 18:33:41 +0800

    version update

--- footer.php
+++ footer.php
@@ -15,13 +15,13 @@
 		<div class="footer">
 		<div class="footer_top">
 			<div class="about_us">
-				<h3>About Us</h3>
-				<p>Innovating is necessary. Simplify too. Bus is a platform created to simplify the process of searching for bus tickets over the internet. With practicality and agility, we want to improve the experience when you, traveler, want to plan your trip. Choose your destination, boarding date and compare among all the options available on our website, which one best suits you. There are more than 5,000 destinations offered throughout Brazil by more than 300 partner companies. So why complicate? With Bus, now it's easy! </p>
+				<h3>Sobre nós</h3>
+				<p>Inovar é preciso. Simplificar também. A Bus é uma plataforma criada para tornar mais simples o processo de pesquisa de passagens de ônibus pela internet. Com praticidade e agilidade, queremos melhorar a experiência na hora em que você, viajante, deseja planejar sua viagem. Escolha seu destino, a data de embarque e compare entre todas as opções disponíveis em nosso site, qual é a que melhor te atende. São mais de 5 mil destinos oferecidos por todo o Brasil por mais de 300 viações parceiras. Então, para quê complicar? Com a Bus, agora ficou fácil! </p>
 			</div>
 		</div>
 		<div class="footer_bottom">
-			<span><a href="<?= get_site_url()?>/privacy-policy">PRIVACY,</a></span> <span><a href="<?= get_site_url()?>/terms-and-conditions">TERMS & CONDITIONS</a></span>
-		 and powered by <span><a href="bus.com.br" target="_blank" rel="noopener noreferrer">BUS.com.br</a></span>
+		<span><a href="<?= get_site_url()?>/privacy-policy">POLÍTICA DE PRIVACIDADE,</a></span> <span><a href="<?= get_site_url()?>/terms-and-conditions">TERMOS E CONDIÇÕES</a></span>
+e desenvolvido por <span><a href="bus.com.br" target="_blank" rel="noopener noreferrer">BUS.com.br</a></span>
 		</div>
 	</div>
 	</footer>
