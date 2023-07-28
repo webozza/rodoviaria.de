614a192ebb80976eab90a5810f6c6f2a82364baa
Sarah Chowdhury <talha@webozzza.com>
2023-07-28 10:52:06 +0600

    test

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,6 +10,25 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div class="single-post">
+                <a href="%s">
+                    <?php $thumbnail_url =  get_the_post_thumbnail_url(get_the_ID(), 'full');
+                    ?>
+                    <img src="<?php echo $thumbnail_url; ?>" alt="Featured Image">
+                    <div class="content-box">
+                        <div class="place-name">
+                            <p><?php the_title(); ?></p>
+                        </div>
+                        <div class="content">
+                            <p><?php include(get_template_directory() . '/content-loop.php'); ?></p>
+                        </div>
+                    </div>
+                    <a class="post-link" href="<?= get_permalink()?>">
+                        <div><img src="<?= get_template_directory_uri()?>/img/icons/botao.png " alt=""></div>
+                    </a>
+                </a>
+            </div>
+
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 		<?php if ( 'post' === get_post_type() ) : ?>
