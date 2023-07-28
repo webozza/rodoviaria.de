fb30efc7700512f234599869193a9867f3b36ca5
Sarah Chowdhury <talha@webozzza.com>
2023-07-28 13:20:24 +0600

    test

--- js/destinations.js
+++ js/destinations.js
@@ -1,4 +1,37 @@
 jQuery(document).ready(function ($) {
-  let loadOn = $(".load-fuck");
-  loadOn.load(`https://www.bus.com.br/cidades/${citySlug}/ .fuck`);
+  // let loadOn = $(".load-fuck");
+  // loadOn.load(`https://www.bus.com.br/cidades/${citySlug}/ .fuck`);
+
+  // function loadTableRows(url, callback) {
+  //   $.get(url, function (response) {
+  //     const rows = $(response).find(".fuck tbody tr");
+  //     callback(rows);
+  //   });
+  // }
+  // let currentPage = 1;
+  // const rowsPerPage = 5;
+
+  // function loadNextRows() {
+  //   let tableBody = $(".load-fuck");
+  //   let startIndex = (currentPage - 1) * rowsPerPage;
+  //   let endIndex = startIndex + rowsPerPage;
+
+  //   let url = `https://www.bus.com.br/cidades/${citySlug}/.fuck`;
+
+  //   loadTableRows(url, function (rows) {
+  //     tableBody.append(rows.slice(startIndex, endIndex));
+
+  //     if (rows.length === 0 || endIndex >= rows.length) {
+  //       $(".loadmore").hide(); // Hide the "Load More" button if there are no more rows to load
+  //     }
+  //   });
+
+  //   currentPage++;
+  // }
+
+  // $(".loadmore").on("click", function () {
+  //   loadNextRows();
+  // });
+
+  // loadNextRows();
 });
